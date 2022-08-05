bbf61063a206153d0702bc0b67a43cd4482e65d2
Edson Martin Feitosa <edsonmfeitosa@hotmail.com>
2022-08-05 10:18:12 -0300

    configuração do git

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,16 @@
+package javaapplication2;
+
+/**
+ *
+ * @author edson
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
